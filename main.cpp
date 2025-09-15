42f3c992b7b914829e289d94c7e513d281847c18
Sergio Ponce Asprilla <131701295+sergiopon@users.noreply.github.com>
2025-09-14 19:37:43 -0500

    refactorizacion del codigo para utilizar la clase Objeto 3D

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@
 #include "glsl.h"
 #include <time.h>
 #include "glm.h"
-#include <FreeImage.h> //*** Para Textura: Incluir librería
+#include <FreeImage.h> //*** Para Textura: Incluir librerï¿½a
+#include "Objeto3D.h" // la nueva clase
+#include <vector>     // vectores para manejar la escena
 
 //-----------------------------------------------------------------------------
 
@@ -18,285 +20,202 @@
 class myWindow : public cwc::glutWindow
 {
 protected:
-   cwc::glShaderManager SM;
-   cwc::glShader *shader;
-   cwc::glShader* shader1; //Para Textura: variable para abrir los shader de textura
-   GLuint ProgramObject;
-   clock_t time0,time1;
-   float timer010;  // timer counting 0->1->0
-   bool bUp;        // flag if counting up or down.
-   GLMmodel* objmodel_ptr;
-   GLMmodel* objmodel_ptr1; //*** Para Textura: variable para objeto texturizado
-   GLMmodel* objmodel_ptr2; //*** Para Textura: variable para objeto texturizado
-   GLuint texid; //*** Para Textura: variable que almacena el identificador de textura
+	// --- Miembros de la Clase Refactorizados ---
+
+	cwc::glShaderManager SM;
+	cwc::glShader* shader;           // Shader para objetos sin textura (ej. con solo color o material)
+	cwc::glShader* shaderTextura;    // Shader para objetos que sï¿½ usan texturas
+	std::vector<Objeto3D> escena; // Un ï¿½nico vector que contiene todos los objetos de nuestra escena.
+	clock_t time0, time1;
+	float timer010;  // timer counting 0->1->0
+	bool bUp;        // flag if counting up or down.
+	GLuint ProgramObject;
 
 
 public:
-	myWindow(){}
+	myWindow() {}
 
-	//*** Para Textura: aqui adiciono un método que abre la textura en JPGa
-	void initialize_textures(void)
+	virtual void OnInit()
 	{
-		int w, h;
-		GLubyte* data = 0;
-		//data = glmReadPPM("soccer_ball_diffuse.ppm", &w, &h);
-		//std::cout << "Read soccer_ball_diffuse.ppm, width = " << w << ", height = " << h << std::endl;
+		// --- 1. Configuraciï¿½n Inicial de OpenGL ---
+		glClearColor(0.5f, 0.5f, 1.0f, 0.0f);
+		glShadeModel(GL_SMOOTH);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_TEXTURE_2D); // Habilitamos el uso de texturas en general
 
-		//dib1 = loadImage("soccer_ball_diffuse.jpg"); //FreeImage
+		// --- 2. Carga de Shaders ---
+		shader = SM.loadfromFile("vertexshader.txt", "fragmentshader.txt");
+		if (shader == 0)
+			std::cout << "Error al cargar el shader bï¿½sico\n";
 
-		glGenTextures(1, &texid);
-		glBindTexture(GL_TEXTURE_2D, texid);
-		glTexEnvi(GL_TEXTURE_2D, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		shaderTextura = SM.loadfromFile("vertexshaderT.txt", "fragmentshaderT.txt");
+		if (shaderTextura == 0)
+			std::cout << "Error al cargar el shader de texturas\n";
 
-		// Loading JPG file
-		FIBITMAP* bitmap = FreeImage_Load(
-			FreeImage_GetFileType("./Mallas/CordylineFREE_BaseColor.png", 0),
-			"./Mallas/CordylineFREE_BaseColor.png");
+		// --- 3. Creaciï¿½n de Objetos y Composiciï¿½n de la Escena ---
 
-		FIBITMAP* pImage = FreeImage_ConvertTo32Bits(bitmap);
-		int nWidth = FreeImage_GetWidth(pImage);
-		int nHeight = FreeImage_GetHeight(pImage);
+		// prueba con objeto natural (Palmera)
+		Objeto3D planta(shaderTextura); // Le decimos que use el shader de texturas
+		planta.cargarMalla("./Mallas/CordylineFREE.obj");
+		planta.agregarTextura("./Mallas/CordylineFREE_BaseColor.png");
+		planta.posX = -1.5f; // Lo movemos a la izquierda
+		planta.escalaX = planta.escalaY = planta.escalaZ = 0.9f; 
+		escena.push_back(planta); // agregamos a la escena
 
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, nWidth, nHeight,
-			0, GL_BGRA, GL_UNSIGNED_BYTE, (void*)FreeImage_GetBits(pImage));
+		
+		// aqui agregar de la misma manera los objetos que se deseen. 
 
-		FreeImage_Unload(pImage);
-		//
-		glEnable(GL_TEXTURE_2D);
+		// --- 4. Inicializaciï¿½n del Timer y la Luz ---
+		time0 = clock();
+		timer010 = 0.0f;
+		bUp = true;
+		DemoLight();
 	}
-
 
 	virtual void OnRender(void)
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-      //float timer010 = 0.09; //for screenshot!
 
-      glPushMatrix();
-	  glRotatef(timer010 * 180, 0, 0.1f, 0);
-	  glTranslatef(0, 0, 0);
+		glPushMatrix();
+		glRotatef(timer010 * 360.0f, 0.0f, 1.0f, 0.0f);
 
-	  printf("Timer: %f \n", timer010);
+		// Se usa un bucle para dibujar laos objetos de la escena
+		for (int i = 0; i < escena.size(); ++i)
+		{
+			escena[i].dibujar(); // Cada objeto sabe cï¿½mo dibujarse a sï¿½ mismo
+		}
 
-      /*if (shader) shader->begin();
-		  
-		  glPushMatrix();
-		  glTranslatef(0, 0, 0);
-		  glmDraw(objmodel_ptr, GLM_SMOOTH | GLM_MATERIAL);
-		  glPopMatrix();
-	      glutSolidTeapot(1.0);
-      if (shader) shader->end();*/
-
-	  //*** Para Textura: llamado al shader para objetos texturizados
-	  if (shader1) shader1->begin();
-		  glPushMatrix();
-		  glTranslatef(0, 0, 0);
-		  glBindTexture(GL_TEXTURE_2D, texid);
-		  glmDraw(objmodel_ptr1, GLM_SMOOTH | GLM_MATERIAL | GLM_TEXTURE);
-		  glPopMatrix();
-	  if (shader1) shader1->end();
-
-
-
-      glutSwapBuffers();
-      glPopMatrix();
-
-      UpdateTimer();
-
+		glPopMatrix();
+		glutSwapBuffers();
+		UpdateTimer();
 		Repaint();
 	}
 
 	virtual void OnIdle() {}
 
-	// When OnInit is called, a render context (in this case GLUT-Window) 
-	// is already available!
-	virtual void OnInit()
+	virtual void OnResize(int w, int h)
 	{
-		glClearColor(0.5f, 0.5f, 1.0f, 0.0f);
-		glShadeModel(GL_SMOOTH);
-		glEnable(GL_DEPTH_TEST);
+		if (h == 0) h = 1;
+		float ratio = 1.0f * (float)w / (float)h;
 
-		shader = SM.loadfromFile("vertexshader.txt","fragmentshader.txt"); // load (and compile, link) from file
-		if (shader==0) 
-         std::cout << "Error Loading, compiling or linking shader\n";
-      else
-      {
-         ProgramObject = shader->GetProgramObject();
-      }
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
 
-	 //*** Para Textura: abre los shaders para texturas
-		shader1 = SM.loadfromFile("vertexshaderT.txt", "fragmentshaderT.txt"); // load (and compile, link) from file
-		if (shader1 == 0)
-			std::cout << "Error Loading, compiling or linking shader\n";
-		else
-		{
-			ProgramObject = shader1->GetProgramObject();
-		}
+		glViewport(0, 0, w, h);
 
-      time0 = clock();
-      timer010 = 0.0f;
-      bUp = true;
-
-	  //Abrir mallas
-	  objmodel_ptr = NULL;
-
-
-	  if (!objmodel_ptr)
-	  {
-		  objmodel_ptr = glmReadOBJ("./Mallas/bunny.obj");
-		  if (!objmodel_ptr)
-			  exit(0);
-
-		  glmUnitize(objmodel_ptr);
-		  glmFacetNormals(objmodel_ptr);
-		  glmVertexNormals(objmodel_ptr, 90.0);
-	  }
-
-
-	  //*** Para Textura: abrir malla de objeto a texturizar
-	  objmodel_ptr1 = NULL;
-
-	  if (!objmodel_ptr1)
-	  {
-		  objmodel_ptr1 = glmReadOBJ("./Mallas/CordylineFREE.obj");
-		  if (!objmodel_ptr1)
-			  exit(0);
-
-		  glmUnitize(objmodel_ptr1);
-		  glmFacetNormals(objmodel_ptr1);
-		  glmVertexNormals(objmodel_ptr1, 90.0);
-	  }
- 
-	  //*** Para Textura: abrir archivo de textura
-	  initialize_textures();
-      DemoLight();
-
+		gluPerspective(45, ratio, 1, 100);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(0.0f, 0.0f, 5.0f, // aumento la distancia de 4 a 5
+			0.0, 0.0, -1.0,
+			0.0f, 1.0f, 0.0f);
 	}
 
-	virtual void OnResize(int w, int h)
-   {
-      if(h == 0) h = 1;
-	   float ratio = 1.0f * (float)w / (float)h;
-
-      glMatrixMode(GL_PROJECTION);
-	   glLoadIdentity();
-	
-	   glViewport(0, 0, w, h);
-
-      gluPerspective(45,ratio,1,100);
-	   glMatrixMode(GL_MODELVIEW);
-	   glLoadIdentity();
-	   gluLookAt(0.0f,0.0f,4.0f, 
-		          0.0,0.0,-1.0,
-			       0.0f,1.0f,0.0f);
-   }
-	virtual void OnClose(void){}
-	virtual void OnMouseDown(int button, int x, int y) {}    
+	virtual void OnClose(void) {}
+	virtual void OnMouseDown(int button, int x, int y) {}
 	virtual void OnMouseUp(int button, int x, int y) {}
-	virtual void OnMouseWheel(int nWheelNumber, int nDirection, int x, int y){}
+	virtual void OnMouseWheel(int nWheelNumber, int nDirection, int x, int y) {}
 
 	virtual void OnKeyDown(int nKey, char cAscii)
-	{       
+	{
 		if (cAscii == 27) // 0x1b = ESC
 		{
 			this->Close(); // Close Window!
-		} 
+		}
 	};
 
 	virtual void OnKeyUp(int nKey, char cAscii)
 	{
-      if (cAscii == 's')      // s: Shader
-         shader->enable();
-      else if (cAscii == 'f') // f: Fixed Function
-         shader->disable();
+		if (cAscii == 's')      // s: Shader
+			shader->enable();
+		else if (cAscii == 'f') // f: Fixed Function
+			shader->disable();
 	}
 
-   void UpdateTimer()
-   {
-      time1 = clock();
-      float delta = static_cast<float>(static_cast<double>(time1-time0)/static_cast<double>(CLOCKS_PER_SEC));
-      delta = delta / 4;
-      if (delta > 0.00005f)
-      {
-         time0 = clock();
-         if (bUp)
-         {
-            timer010 += delta;
-            if (timer010>=1.0f) { timer010 = 1.0f; bUp = false;}
-         }
-         else
-         {
-            timer010 -= delta;
-            if (timer010<=0.0f) { timer010 = 0.0f; bUp = true;}
-         }
-      }
-   }
+	void UpdateTimer()
+	{
+		time1 = clock();
+		float delta = static_cast<float>(static_cast<double>(time1 - time0) / static_cast<double>(CLOCKS_PER_SEC));
+		delta = delta / 4;
+		if (delta > 0.00005f)
+		{
+			time0 = clock();
+			if (bUp)
+			{
+				timer010 += delta;
+				if (timer010 >= 1.0f) { timer010 = 1.0f; bUp = false; }
+			}
+			else
+			{
+				timer010 -= delta;
+				if (timer010 <= 0.0f) { timer010 = 0.0f; bUp = true; }
+			}
+		}
+	}
 
-   void DemoLight(void)
-   {
-     glEnable(GL_LIGHTING);
-     glEnable(GL_LIGHT0);
-     glEnable(GL_NORMALIZE);
-     
-     /*// Light model parameters:
-     // -------------------------------------------
-     
-     GLfloat lmKa[] = {0.0, 0.0, 0.0, 0.0 };
-     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmKa);
-     
-     glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, 1.0);
-     glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 0.0);
-     
-     // -------------------------------------------
-     // Spotlight Attenuation
-     
-     GLfloat spot_direction[] = {1.0, -1.0, -1.0 };
-     GLint spot_exponent = 30;
-     GLint spot_cutoff = 180;
-     
-     glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
-     glLighti(GL_LIGHT0, GL_SPOT_EXPONENT, spot_exponent);
-     glLighti(GL_LIGHT0, GL_SPOT_CUTOFF, spot_cutoff);
-    
-     GLfloat Kc = 1.0;
-     GLfloat Kl = 0.0;
-     GLfloat Kq = 0.0;
-     
-     glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION,Kc);
-     glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, Kl);
-     glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, Kq);
-     
-     
-     // ------------------------------------------- 
-     // Lighting parameters:
+	void DemoLight(void)
+	{
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+		glEnable(GL_NORMALIZE);
 
-     GLfloat light_pos[] = {0.0f, 5.0f, 5.0f, 1.0f};
-     GLfloat light_Ka[]  = {1.0f, 0.5f, 0.5f, 1.0f};
-     GLfloat light_Kd[]  = {1.0f, 0.1f, 0.1f, 1.0f};
-     GLfloat light_Ks[]  = {1.0f, 1.0f, 1.0f, 1.0f};
+		/*// Light model parameters:
+		// -------------------------------------------
 
-     glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-     glLightfv(GL_LIGHT0, GL_AMBIENT, light_Ka);
-     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_Kd);
-     glLightfv(GL_LIGHT0, GL_SPECULAR, light_Ks);
+		GLfloat lmKa[] = { 0.0, 0.0, 0.0, 0.0 };
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmKa);
 
-     // -------------------------------------------
-     // Material parameters:
+		glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, 1.0);
+		glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, 0.0);
 
-     GLfloat material_Ka[] = {0.5f, 0.0f, 0.0f, 1.0f};
-     GLfloat material_Kd[] = {0.4f, 0.4f, 0.5f, 1.0f};
-     GLfloat material_Ks[] = {0.8f, 0.8f, 0.0f, 1.0f};
-     GLfloat material_Ke[] = {0.1f, 0.0f, 0.0f, 0.0f};
-     GLfloat material_Se = 20.0f;
+		// -------------------------------------------
+		// Spotlight Attenuation
 
-     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_Ka);
-     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_Kd);
-     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material_Ks);
-     glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, material_Ke);
-     glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_Se);*/
-   }
+		GLfloat spot_direction[] = { 1.0, -1.0, -1.0 };
+		GLint spot_exponent = 30;
+		GLint spot_cutoff = 180;
+
+		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
+		glLighti(GL_LIGHT0, GL_SPOT_EXPONENT, spot_exponent);
+		glLighti(GL_LIGHT0, GL_SPOT_CUTOFF, spot_cutoff);
+
+		GLfloat Kc = 1.0;
+		GLfloat Kl = 0.0;
+		GLfloat Kq = 0.0;
+
+		glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, Kc);
+		glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, Kl);
+		glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, Kq);
+
+
+		// -------------------------------------------
+		// Lighting parameters:
+
+		GLfloat light_pos[] = { 0.0f, 5.0f, 5.0f, 1.0f };
+		GLfloat light_Ka[] = { 1.0f, 0.5f, 0.5f, 1.0f };
+		GLfloat light_Kd[] = { 1.0f, 0.1f, 0.1f, 1.0f };
+		GLfloat light_Ks[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+		glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+		glLightfv(GL_LIGHT0, GL_AMBIENT, light_Ka);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_Kd);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, light_Ks);
+
+		// -------------------------------------------
+		// Material parameters:
+
+		GLfloat material_Ka[] = { 0.5f, 0.0f, 0.0f, 1.0f };
+		GLfloat material_Kd[] = { 0.4f, 0.4f, 0.5f, 1.0f };
+		GLfloat material_Ks[] = { 0.8f, 0.8f, 0.0f, 1.0f };
+		GLfloat material_Ke[] = { 0.1f, 0.0f, 0.0f, 0.0f };
+		GLfloat material_Se = 20.0f;
+
+		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_Ka);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_Kd);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material_Ks);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, material_Ke);
+		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material_Se);*/
+	}
 };
 
 //-----------------------------------------------------------------------------
